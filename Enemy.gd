802ed9794bdfea6a815beb62f7c6f8d2b3de8bb1
Gerald <geraldastudillo1234@gmail.com>
2023-02-12 14:14:16 -0500

    Animar el salto

--- Enemy.gd
+++ Enemy.gd
@@ -1,16 +1,19 @@
 extends KinematicBody2D
 var velocity = Vector2.ZERO
 var mov = true
-
+var gravity = 200;
 func _physics_process(delta):
+	velocity.y += gravity * delta
+
 	if is_on_wall():
 		mov = not mov
 	if mov==true:
 		velocity.x = 50
 	else:
 		velocity.x = -50
-	velocity = move_and_slide(velocity, Vector2.UP)
 
+
+	velocity = move_and_slide(velocity,Vector2.UP)
 
 func _on_Area2D_area_entered(area):
 	if area.is_in_group("player"):
--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -6,8 +6,7 @@ var max_speed = 150
 var friction = 0.5
 var resistance = 0.5
 var timer
-export(bool) var dashcomp : bool
-var dash : bool  
+var big : bool = false
 export var cooldown = 3
 onready var sprite = $Sprite
 onready var collision = $CollisionShape2D
@@ -16,25 +15,43 @@ func norm():
 	collision2.scale.x=1
 	collision2.scale.y=1
 	collision.scale.x=1
-	sprite.scale.x=1
+	sprite.scale.x=0.569
 	collision.scale.y=1
-	sprite.scale.y=1
+	sprite.scale.y=0.569
+	$Sprite2.scale.y=0.569
+	$Sprite2.scale.x=0.569
 func invc():
 	collision2.scale.x=0
 	collision2.scale.y=0
-	sprite.scale.x=1
-	sprite.scale.y=1
 func big():
-	collision2.scale.x=2
-	collision2.scale.y=-2
-	collision.scale.x=2
-	sprite.scale.x=2
-	collision.scale.y=-2
-	sprite.scale.y=-2
+	collision2.scale.x=2.5
+	collision2.scale.y=1.99
+	collision.scale.x=2.5
+	sprite.scale.x=1.70
+	collision.scale.y=1.90
+	sprite.scale.y=1
+	$Sprite2.scale.y=1
+	$Sprite2.scale.x=1.70
+	big = true 
 func jump():
 	velocity.y -= 300
 func _physics_process(delta):
+	$Sprite/AnimationPlayer.play("Saltar")
 	var movement_x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	if  Input.is_action_pressed("ui_left"):
+		if big== true:
+			sprite.scale.x=-1.70
+			$Sprite2.scale.x=-1.70
+		else:
+			sprite.scale.x=-0.569
+			$Sprite2.scale.x=-0.569
+	if  Input.is_action_pressed("ui_right"):
+		if big== true:
+			sprite.scale.x=1.70
+			$Sprite2.scale.x=1.70
+		else:
+			sprite.scale.x=0.569
+			$Sprite2.scale.x=0.569
 	if movement_x != 0:
 		velocity.x += movement_x * speed * delta
 		velocity.x = clamp(velocity.x, -max_speed, max_speed)
@@ -49,8 +66,17 @@ func _physics_process(delta):
 		if movement_x == 0:
 			velocity.x = lerp(velocity.x, 0, resistance)
 	if is_on_floor():
+		
 		if Input.is_action_just_pressed("ui_up"):
 			jump()
+			$Sprite2.visible = true
+			$Sprite.visible = false
+			$Sprite/AnimationPlayer.stop(true)
+			$Sprite2/AnimationPlayer.play("jump")
+		else:
+			$Sprite.visible = true
+			$Sprite2.visible = false
+			
 
 	velocity.y += gravity * delta
 
@@ -66,3 +92,4 @@ func _on_Area2D_area_entered(area):
 		$Timer.start()
 func _on_Timer_timeout():
 	norm()
+	big = false
